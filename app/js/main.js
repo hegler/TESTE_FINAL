307504c42489bfb4ab844ee6901673bc6d4c4074
Alessandro Ribeiro <alessandrohbnr@gmail.com>
2016-04-10 19:33:09 -0300

    Add JS in project

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,28 @@
-$(document).ready(function(){
-    console.log('its working !');
-})
+/**
+ * Load and config List plugin on messages page
+ */
+(function($, List) {
+    "use strict";
+
+    function loadList() {
+        // set field to filter
+        var options = {
+            valueNames: [ 'media-heading', 'media-content' ]
+        };
+
+        // create a new instance of component
+        return new List('content', options);
+    }
+    
+    // load List plugin
+    var list = loadList();
+
+    // remove message using List plugin
+    $('.btn-remove').click(function(e) {
+        e.preventDefault();
+
+        $('.list input:checked').each(function() {
+            list.remove('media-heading', $(this).parent().find('.media-heading').html());
+        });
+    });
+})(jQuery, List);
